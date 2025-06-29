805e03f10b8d6e8a2f9e34cf870468911c3a19f8
Ali AlSaghir <alisalsaghir@gmail.com>
2025-06-29 16:06:19 +0300

    complete genre functionality

--- models/Genre.php
+++ models/Genre.php
@@ -16,6 +16,18 @@ class Genre extends Model {
     }
   }
 
+  public function getId(): int {
+    return $this->id;
+  }
+
+  public function getName(): string {
+    return $this->name;
+  }
+
+  public function setName(string $name): void {
+    $this->name = $name;
+  }
+
   public function toArray(): array {
     return [
       'id' => $this->id,
